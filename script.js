6f7a02013cbfe63f69a0e2db6db840beb52807eb
Diarit-S <diarit-94@hotmail.fr>
2018-11-14 10:57:14 +0100

    Premières images

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ var open = document.getElementById('open');
 open.addEventListener('click', function() {
   nav.classList.toggle('is-open');
 });
+
+
+
+var item = document.getElementById('item');
+var item2 = document.getElementById('item2');
+
+item.addEventListener('click', function() {
+	item2.classListe.toggle('item-is-open');
+});
